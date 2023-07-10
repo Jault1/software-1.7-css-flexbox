98e09a400d31b17ceae55d84ebcb4b2f913816ac
John Ault <john.ault.dev@gmail.com>
2023-07-10 10:47:26 -0400

    Pushing up decent template

---
+++ action_page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Css Flexbox</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/js/jquery-3.4.1.min.js">
+
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <header>
+        <h1>My Portfolio</h1>
+        <h2>John Ault</h2>
+    </header>
+
+    <section class="row">
+        <h3>Thanks <?php echo $_POST["name"]; ?>, for reaching out. </h3>
+            <p>I'll get back to you soon.</p>
+    </section>
+
+    
+</body>
+</html>
